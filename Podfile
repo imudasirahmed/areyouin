5fb4809f391512eeceaca63146ca2e3744a756e6
Mudasir Ahmed <mudasir.ahmed@united.com>
2018-07-12 17:26:16 -0500

    [Mudasir] First Commit

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'areyouin' do
+  use_frameworks!
+
+  # Pods for areyouin
+  pod 'Firebase/Auth'
+  pod 'Firebase/Database'
+  pod 'Firebase/Storage'
+  pod 'Firebase/Core'
+
+end
---
+++ areyouin/HomeController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  areyouin
+//
+//  Created by Mudasir Ahmed on 7/12/18.
+//  Copyright © 2018 Mudasir Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
