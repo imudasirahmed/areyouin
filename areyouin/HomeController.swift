796ce6b0f250b91c83c2e832cca399df89e99099
Mudasir Ahmed <mudasir.ahmed@united.com>
2018-07-13 11:41:42 -0500

    [Mudasir] added tab bar icons, fixed Asset catalog  that was previously deleted

--- areyouin/HomeController.swift
+++ areyouin/HomeController.swift
@@ -12,14 +12,13 @@ class HomeController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupNavigationItems()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupNavigationItems() {
+        navigationItem.titleView = UIImageView(image:#imageLiteral(resourceName: "logo-blue"))
     }
-
 
 }
 
