3a4f4071d2806ef90d400154b8aa8d7403347554
Mudasir Ahmed <mudasir.ahmed@united.com>
2018-07-13 14:39:03 -0500

    [Mudasir] added firebase pod

---
+++ areyouin/MainTabController.swift
@@ -0,0 +1,16 @@
+//
+//  MainTabController.swift
+//  areyouin
+//
+//  Created by Mudasir Ahmed on 7/13/18.
+//  Copyright © 2018 Mudasir Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
