c66f8799379a6ae01a9295718b42e65a890b613e
Mudasir Ahmed <mudasir.ahmed@united.com>
2018-07-30 13:45:58 -0500

    [Mudasir] added constraints extension

--- areyouin/RegistrationViewController.swift
+++ areyouin/RegistrationViewController.swift
@@ -12,7 +12,6 @@ class RegistrationViewController: UIViewController {
 
     let plusPhotoButton:UIButton = {
         let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
         button.setImage(#imageLiteral(resourceName: "plus_photo").withRenderingMode(UIImageRenderingMode.alwaysOriginal), for: UIControlState.normal)
         return button
     }()
@@ -21,7 +20,6 @@ class RegistrationViewController: UIViewController {
         let textField = UITextField()
         textField.placeholder = "Email"
         textField.borderStyle = .roundedRect
-        textField.translatesAutoresizingMaskIntoConstraints = false
         textField.backgroundColor = UIColor.init(white: 0, alpha: 0.03)
         textField.font = UIFont.systemFont(ofSize: 14)
         return textField
@@ -31,7 +29,6 @@ class RegistrationViewController: UIViewController {
         let textField = UITextField()
         textField.placeholder = "Username"
         textField.borderStyle = .roundedRect
-        textField.translatesAutoresizingMaskIntoConstraints = false
         textField.backgroundColor = UIColor.init(white: 0, alpha: 0.03)
         textField.font = UIFont.systemFont(ofSize: 14)
         return textField
@@ -42,7 +39,6 @@ class RegistrationViewController: UIViewController {
         textField.placeholder = "Password"
         textField.borderStyle = .roundedRect
         textField.isSecureTextEntry = true
-        textField.translatesAutoresizingMaskIntoConstraints = false
         textField.backgroundColor = UIColor.init(white: 0, alpha: 0.03)
         textField.font = UIFont.systemFont(ofSize: 14)
         return textField
@@ -51,7 +47,7 @@ class RegistrationViewController: UIViewController {
     let signUpButton:UIButton = {
        let button = UIButton.init(type: UIButtonType.system)
         button.setTitle("Sign Up", for: UIControlState.normal)
-        button.backgroundColor = UIColor.init(red: 149/255, green: 204/255, blue: 244/255, alpha: 1)
+        button.backgroundColor = UIColor.rgb(red: 149, green: 204, blue: 244)
         button.layer.cornerRadius = 5
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
         button.setTitleColor(UIColor.white, for: UIControlState.normal)
@@ -65,28 +61,21 @@ class RegistrationViewController: UIViewController {
         
         view.addSubview(plusPhotoButton)
         
-        plusPhotoButton.heightAnchor.constraint(equalToConstant: 140).isActive = true
-        plusPhotoButton.widthAnchor.constraint(equalToConstant: 140).isActive = true
+        plusPhotoButton.anchor(top: view.topAnchor, left: nil, bottom: nil, right: nil, paddingTop: 40, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 140, height: 140)
         plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        plusPhotoButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
-        
+
         setupInputFields()
     }
     
     fileprivate func setupInputFields() {
         
         let stackView = UIStackView.init(arrangedSubviews: [emailTextField, userNameTextField, passwordTextField, signUpButton])
-        stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.distribution = .fillEqually
         stackView.axis = .vertical
         stackView.spacing = 10
         view.addSubview(stackView)
         
-        NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: plusPhotoButton.bottomAnchor, constant: 20),
-            stackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40),
-            stackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40),
-            stackView.heightAnchor.constraint(equalToConstant: 200)
-            ])
+        stackView.anchor(top: plusPhotoButton.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 20, paddingLeft: 40, paddingBottom: 0, paddingRight: 40, width: 0, height: 200)
+
     }
 }
